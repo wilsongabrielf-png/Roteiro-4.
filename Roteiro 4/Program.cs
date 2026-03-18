dfd53bb0d1e17267820d87bd4a313854700c9eeb
wilsongabrielf-png <wilsongabrielf@gmail.com>
2026-03-18 20:39:24 -0300

    Add files via upload

---
+++ Roteiro 4/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+
+class Pagamento
+{
+    public virtual void ProcessarPagamento()
+    {
+        Console.WriteLine("Processando pagamento genérico...");
+    }
+}
+
+class CartaoCredito : Pagamento
+{
+    public override void ProcessarPagamento()
+    {
+        Console.WriteLine("Processando pagamento com Cartão de Crédito...");
+    }
+}
+
+class Pix : Pagamento
+{
+    public override void ProcessarPagamento()
+    {
+        Console.WriteLine("Processando pagamento via Pix...");
+    }
+}
+
+class BoletoBancario : Pagamento
+{
+    public override void ProcessarPagamento()
+    {
+        Console.WriteLine("Processando pagamento via Boleto Bancário...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Pagamento> pagamentos = new List<Pagamento>();
+
+        pagamentos.Add(new CartaoCredito());
+        pagamentos.Add(new BoletoBancario());
+        pagamentos.Add(new Pix());
+
+        foreach (Pagamento pagamento in pagamentos)
+        {
+            pagamento.ProcessarPagamento();
+        }
+    }
+}
+
+
+
---
+++ Roteiro 4/Roteiro 4.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+abstract class Funcionario
+{
+    public string Nome { get; set; }
+    public double SalarioBase { get; set; }
+
+    public Funcionario(string nome, double salarioBase)
+    {
+        Nome = nome;
+        SalarioBase = salarioBase;
+    }
+
+    public abstract double CalcularSalario();
+}
+
+class Gerente : Funcionario
+{
+    public double Bonus { get; set; }
+
+    public Gerente(string nome, double salarioBase, double bonus)
+        : base(nome, salarioBase)
+    {
+        Bonus = bonus;
+    }
+
+    public override double CalcularSalario()
+    {
+        return SalarioBase + Bonus;
+    }
+}
+
+class Programador : Funcionario
+{
+    public int HorasExtras { get; set; }
+    public double ValorHoraExtra { get; set; }
+
+    public Programador(string nome, double salarioBase, int horasExtras, double valorHoraExtra)
+        : base(nome, salarioBase)
+    {
+        HorasExtras = horasExtras;
+        ValorHoraExtra = valorHoraExtra;
+    }
+
+    public override double CalcularSalario()
+    {
+        return SalarioBase + (HorasExtras * ValorHoraExtra);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Funcionario gerente = new Gerente("Carlos", 5000, 1500);
+        Funcionario programador = new Programador("Ana", 4000, 20, 50);
+
+        Console.WriteLine($"Salário do gerente: {gerente.CalcularSalario()}");
+        Console.WriteLine($"Salário do programador: {programador.CalcularSalario()}");
+    }
+}
